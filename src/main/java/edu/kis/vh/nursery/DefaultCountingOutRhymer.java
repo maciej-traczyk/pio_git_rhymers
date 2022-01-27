c52aca88eb0186ac08d64416c20ac3787b51a1a2
Maciej Traczyk <228874@edu.p.lodz.pl>
2022-01-27 15:39:20 +0100

    1.4 accesory 2

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -8,6 +8,10 @@ public class DefaultCountingOutRhymer {
 
     private int total = ERROR;
 
+    public int getTotal() {
+        return total;
+    }
+
     public void countIn(int in) {
         if (!isFull())
             numbers[++total] = in;
